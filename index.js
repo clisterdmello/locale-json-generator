1fd8cc63b74b487b156fb012cfcac2194a4e6926
clisterdmello@gmail.com <clisterdmello@gmail.com>
2018-09-30 22:48:05 +0200

    First Set of Translation generator.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 #!/usr/bin/env node
 
-console.log('My first Package');
+const excelToJson = require('convert-excel-to-json');
+const excelpath = './source/translations.xlsx';
+const jsonData = excelToJson({
+    sourceFile: excelpath
+});
+const currentSheet = Object.keys(jsonData)[0];
+const JsonOfCurrentSheet = jsonData[currentSheet];
+const headerRow = JsonOfCurrentSheet[0];
+let tranlationList = [];
+
+Object.keys(headerRow).forEach(function (col) {
+    if(headerRow[col] !=='key'){
+        tranlationList[headerRow[col]] = {};
+    }    
+});
+
+
+for (let row = 1; row < JsonOfCurrentSheet.length; row++) {
+    let jsonKey = null;
+
+    Object.keys(headerRow).forEach(function (col) {
+        if(headerRow[col] ==='key'){
+            jsonKey = JsonOfCurrentSheet[row][col];
+        }else{
+            let currentTranslation = {};
+                currentTranslation [jsonKey] = JsonOfCurrentSheet[row][col];
+                Object.assign(tranlationList[headerRow[col]],currentTranslation);
+        }
+    });
+}
+console.log(tranlationList);
