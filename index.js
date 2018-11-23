065711b2d701f6009946ba00b40577a3307d94ad
Clister Dmello <clisterdmello@gmail.com>
2018-11-23 15:38:27 +0100

    Update index.js
    this creates the JOSN file for EN, NL, FR, GE

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
 const excelToJson = require('convert-excel-to-json');
-const excelpath = './source/translations.xlsx';
+const SOURCEEXCELFILE = path.resolve(__dirname, `../../${process.argv[2]}`);
+const DESTINATION = path.resolve(__dirname, `../../${process.argv[3]}`);
+const writeJsonFile = require('write-json-file');
+
 const jsonData = excelToJson({
-    sourceFile: excelpath
+    sourceFile: SOURCEEXCELFILE
 });
 const currentSheet = Object.keys(jsonData)[0];
 const JsonOfCurrentSheet = jsonData[currentSheet];
@@ -11,23 +16,33 @@ const headerRow = JsonOfCurrentSheet[0];
 let tranlationList = [];
 
 Object.keys(headerRow).forEach(function (col) {
-    if(headerRow[col] !=='key'){
+    if (headerRow[col] !== 'key') {
         tranlationList[headerRow[col]] = {};
-    }    
+    }
 });
 
-
-for (let row = 1; row < JsonOfCurrentSheet.length; row++) {
-    let jsonKey = null;
+for (let row = 2; row < JsonOfCurrentSheet.length; row++) {
+    let jsonKey;
 
     Object.keys(headerRow).forEach(function (col) {
-        if(headerRow[col] ==='key'){
+        if (headerRow[col] === 'Label') {
             jsonKey = JsonOfCurrentSheet[row][col];
-        }else{
+        } else if (jsonKey !== undefined) {
             let currentTranslation = {};
-                currentTranslation [jsonKey] = JsonOfCurrentSheet[row][col];
-                Object.assign(tranlationList[headerRow[col]],currentTranslation);
+            currentTranslation[jsonKey] = JsonOfCurrentSheet[row][col];
+            Object.assign(tranlationList[headerRow[col]], currentTranslation);
         }
     });
 }
-console.log(tranlationList);
+
+
+Object.keys(tranlationList).forEach((key) => {
+    let path;
+    if (key.match(/EN|NL|FR|DE/) !== null) {
+        path = `${DESTINATION}/${key.toLowerCase()}.json`;
+        // fs.unlink(path);
+        writeJsonFile(path, tranlationList[key])
+        .then(() => console.log('Translation Done for:', key.toLowerCase()))
+        .catch(() => console.log('Translation missing for:', key.toLowerCase()))
+    }
+});
